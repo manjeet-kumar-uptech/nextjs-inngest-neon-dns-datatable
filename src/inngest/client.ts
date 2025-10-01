39c6fdad8554592324ad78cb098d901608abe85e
Manjeet Kumar <manjeet@uptics.io>
2025-10-01 15:21:12 +0530

    Check if changing key name works

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -5,7 +5,7 @@ export const inngest = new Inngest({
   id: "nextjs-app",
   name: "Next.js Inngest App",
   eventKey: process.env.INNGEST_EVENT_KEY,
-  signingKey: process.env.INNGEST_SIGNING_KEY,
+  apiKey: process.env.INNGEST_SIGNING_KEY,
   
   // In development, Inngest will automatically connect to the Inngest Dev Server
   // In production, you'll need to set the INNGEST_EVENT_KEY and INNGEST_SIGNING_KEY environment variables
